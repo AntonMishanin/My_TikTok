01e3480cadcc2335022bd426deeeb51e2941902a
Anton Mishanin <antonmishanin@mail.ru>
2020-12-21 20:00:58 +0700

    init profile feature

---
+++ profile-feature/src/main/java/com/example/profile_feature/ProfileFragment.kt
@@ -0,0 +1,9 @@
+package com.example.profile_feature
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+
+class ProfileFragment: Fragment(R.layout.fragment_progile) {
+
+    private val viewModel: ProfileViewModel by viewModels()
+}
---
+++ profile-feature/src/main/java/com/example/profile_feature/ProfileViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.profile_feature
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel: ViewModel() {
+}
