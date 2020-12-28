43fc41ad5cc66cf81715329eddfba1c6f7b27af2
Anton Mishanin <antonmishanin@mail.ru>
2020-12-28 10:02:45 +0700

    Settings: add navigator

--- settings-feature/src/main/java/com/example/settings_feature/SettingsFragment.kt
+++ settings-feature/src/main/java/com/example/settings_feature/SettingsFragment.kt
@@ -2,8 +2,10 @@ package com.example.settings_feature
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.settings_feature.navigator.SettingsNavigator
 
 class SettingsFragment: Fragment(R.layout.fragment_settings) {
 
@@ -11,5 +13,16 @@ class SettingsFragment: Fragment(R.layout.fragment_settings) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val navigator = requireActivity() as SettingsNavigator
+
+        val backButton = requireView().findViewById<Button>(R.id.button_back)
+        backButton.setOnClickListener{
+            navigator.onClickBack()
+        }
     }
 }
---
+++ settings-feature/src/main/java/com/example/settings_feature/navigator/SettingsNavigator.kt
@@ -0,0 +1,8 @@
+package com.example.settings_feature.navigator
+
+interface SettingsNavigator {
+
+    fun onClickBack()
+
+    fun onLogOut()
+}
