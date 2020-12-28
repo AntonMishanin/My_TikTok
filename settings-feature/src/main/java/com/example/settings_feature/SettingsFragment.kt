6b07d076873d03a61ad2480231f8806de41904a1
Anton Mishanin <antonmishanin@mail.ru>
2020-12-28 20:45:10 +0700

    Settings: 1.Added database init. 2. Exit

--- settings-feature/src/main/java/com/example/settings_feature/SettingsFragment.kt
+++ settings-feature/src/main/java/com/example/settings_feature/SettingsFragment.kt
@@ -1,28 +1,43 @@
 package com.example.settings_feature
 
+import android.content.Context
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.data.RepositoryImpl
+import com.example.data.database.UserDatabase
+import com.example.data.preferences.SharedPref
 import com.example.settings_feature.navigator.SettingsNavigator
 
 class SettingsFragment: Fragment(R.layout.fragment_settings) {
 
     private val viewModel: SettingsViewModel by viewModels()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         val navigator = requireActivity() as SettingsNavigator
 
+        val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        val sharedPref = SharedPref(sharedPreferences)
+
+        val userDatabase =
+            UserDatabase.getUserDatabase(requireContext().applicationContext)
+        val userDao = userDatabase.getUserDao()
+
+        val repository = RepositoryImpl(sharedPref, userDao)
+
+        viewModel.onViewCreated(repository, navigator)
+
         val backButton = requireView().findViewById<Button>(R.id.button_back)
         backButton.setOnClickListener{
-            navigator.onClickBack()
+            viewModel.onClickBack()
+        }
+
+        val exitButton = requireView().findViewById<Button>(R.id.button_exit_settings)
+        exitButton.setOnClickListener{
+            viewModel.onClickExitButton()
         }
     }
 }
--- settings-feature/src/main/java/com/example/settings_feature/SettingsViewModel.kt
+++ settings-feature/src/main/java/com/example/settings_feature/SettingsViewModel.kt
@@ -1,6 +1,25 @@
 package com.example.settings_feature
 
 import androidx.lifecycle.ViewModel
+import com.example.data.RepositoryImpl
+import com.example.settings_feature.navigator.SettingsNavigator
 
-class SettingsViewModel: ViewModel() {
+class SettingsViewModel : ViewModel() {
+
+    private lateinit var repository: RepositoryImpl
+    private lateinit var navigator: SettingsNavigator
+
+    fun onViewCreated(repository: RepositoryImpl, navigator: SettingsNavigator) {
+        this.repository = repository
+        this.navigator = navigator
+    }
+
+    fun onClickExitButton() {
+        repository.setToken(-1L)
+        navigator.onLogOut()
+    }
+
+    fun onClickBack() {
+        navigator.onClickBack()
+    }
 }
