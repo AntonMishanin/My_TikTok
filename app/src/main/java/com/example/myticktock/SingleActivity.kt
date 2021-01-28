fea6a10e0b6846e8d9880ca4e00d59bd74552c52
Anton Mishanin <antonmishanin@mail.ru>
2021-01-28 20:48:44 +0700

    Add animation of transitions between fragments

--- app/src/main/java/com/example/myticktock/SingleActivity.kt
+++ app/src/main/java/com/example/myticktock/SingleActivity.kt
@@ -3,6 +3,7 @@ package com.example.myticktock
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import com.example.authorization_feature.navigator.AuthorizationNavigator
 import com.example.bottom_navigation_feature.navigator.BottomNavigator
@@ -17,14 +18,18 @@ class SingleActivity : AppCompatActivity(),
     BottomNavigator, SettingsNavigator,
     VideoNavigator, EditProfileNavigator {
 
-
     private lateinit var navController: NavController
+
+    private lateinit var navBuilder: NavOptions.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single)
 
         navController = findNavController(R.id.nav_host_fragment)
+        navBuilder = NavOptions.Builder()
+        navBuilder.setEnterAnim(R.anim.wait).setExitAnim(R.anim.wait)
+            .setPopEnterAnim(R.anim.wait).setPopExitAnim(R.anim.wait)
     }
 
     /*
@@ -32,22 +37,22 @@ class SingleActivity : AppCompatActivity(),
      */
 
     override fun navigateToAuthorization() {
-        navController.navigate(R.id.loginFragment)
+        navController.navigate(R.id.loginFragment, null, navBuilder.build())
     }
 
     override fun navigateToProfile() {
-        navController.navigate(R.id.mainFragment)
+        navController.navigate(R.id.mainFragment, null, navBuilder.build())
     }
 
     /*
     Auth
      */
     override fun navigateToLogin() {
-        navController.navigate(R.id.loginFragment)
+        navController.navigate(R.id.loginFragment, null, navBuilder.build())
     }
 
     override fun navigateToRegistration() {
-        navController.navigate(R.id.registrationFragment)
+        navController.navigate(R.id.registrationFragment, null, navBuilder.build())
     }
 
     /*
@@ -55,15 +60,15 @@ class SingleActivity : AppCompatActivity(),
      */
 
     override fun onClickEditProfile() {
-        navController.navigate(R.id.editProfileFragment)
+        navController.navigate(R.id.editProfileFragment, null, navBuilder.build())
     }
 
     override fun onClickSettings() {
-        navController.navigate(R.id.settingsFragment)
+        navController.navigate(R.id.settingsFragment, null, navBuilder.build())
     }
 
     override fun onClickVideo() {
-        navController.navigate(R.id.recordVideoFragment)
+        navController.navigate(R.id.recordVideoFragment, null, navBuilder.build())
     }
 
     /*
@@ -75,7 +80,7 @@ class SingleActivity : AppCompatActivity(),
     }
 
     override fun onLogOut() {
-        navController.navigate(R.id.loginFragment)
+        navController.navigate(R.id.loginFragment, null, navBuilder.build())
     }
 
     /*
