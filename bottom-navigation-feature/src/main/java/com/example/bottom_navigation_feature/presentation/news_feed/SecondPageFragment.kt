79edc0221e74c9df3ab7340eec297b489b242926
Anton Mishanin <antonmishanin@mail.ru>
2021-02-06 10:15:35 +0700

    Material tabs with ViewPager2

---
+++ bottom-navigation-feature/src/main/java/com/example/bottom_navigation_feature/presentation/news_feed/SecondPageFragment.kt
@@ -0,0 +1,7 @@
+package com.example.bottom_navigation_feature.presentation.news_feed
+
+import androidx.fragment.app.Fragment
+import com.example.bottom_navigation_feature.R
+
+class SecondPageFragment: Fragment(R.layout.fragment_second) {
+}
