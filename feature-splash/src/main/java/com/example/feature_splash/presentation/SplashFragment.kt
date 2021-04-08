de00f24e4164338fdada1be894dff164a8d49660
Anton Mishanin <antonmishanin@mail.ru>
2021-04-08 22:43:46 +0700

    Finish custom view.

--- feature-splash/src/main/java/com/example/feature_splash/presentation/SplashFragment.kt
+++ feature-splash/src/main/java/com/example/feature_splash/presentation/SplashFragment.kt
@@ -1,35 +1,71 @@
 package com.example.feature_splash.presentation
 
-import android.graphics.BitmapFactory
+import android.annotation.SuppressLint
+import android.graphics.Matrix
 import android.os.Bundle
-import android.view.View
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
-import com.example.feature_splash.MyView
 import com.example.feature_splash.R
+import com.example.feature_splash.RotationGestureDetector
 import com.example.feature_splash.navigation.SplashNavigator
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
-class SplashFragment : Fragment(R.layout.fragment_splash) {
 
-   @Inject
-   lateinit var viewModelFactory: ViewModelProvider.Factory
+class SplashFragment : Fragment(R.layout.fragment_splash),
+    RotationGestureDetector.OnRotationGestureListener {
 
-   private val viewModel: SplashViewModel by viewModels { viewModelFactory }
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    private val viewModel: SplashViewModel by viewModels { viewModelFactory }
+
+    private var mRotationDetector: RotationGestureDetector? = null
+
+    private var imageView: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidSupportInjection.inject(this)
         super.onCreate(savedInstanceState)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onStart() {
         super.onStart()
 
         val navigator = requireActivity() as SplashNavigator
         viewModel.loadToken(navigator)
 
-        val myView = view?.findViewById<MyView>(R.id.my_view)
+        imageView = view?.findViewById(R.id.rotated_image)
+
+        mRotationDetector = RotationGestureDetector(
+            this,
+            (imageView?.drawable?.intrinsicWidth ?: 0) / 2f,
+            (imageView?.drawable?.intrinsicHeight ?: 0) / 2f,
+            0f,
+            1.5f
+        )
+
+        imageView?.setOnTouchListener { _, event ->
+            mRotationDetector?.onTouchEvent(event) ?: true
+        }
+    }
+
+    override fun OnRotation(rotationDetector: RotationGestureDetector?) {
+        val angle = rotationDetector!!.angle
+
+        val matrix = Matrix()
+        matrix.postRotate(
+            angle,
+            (imageView?.drawable?.intrinsicWidth ?: 0) / 2f,
+            (imageView?.drawable?.intrinsicHeight ?: 0) / 2f
+        )
+        val rotatedValues = FloatArray(9)
+        matrix.getValues(rotatedValues)
+
+        val sourceMatrix = imageView?.matrix
+        sourceMatrix?.setValues(rotatedValues)
+        imageView?.imageMatrix = sourceMatrix
     }
 }
